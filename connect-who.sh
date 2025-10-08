28561ae6e4cbc895b86d76309659f4b8611e7403
Dipak Thapa Magar <46467788+dipakthapamagar@users.noreply.github.com>
2025-10-08 10:47:44 +0545

    Create connect-who.sh

---
+++ connect-who.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+RED=$(tput setaf 1; tput bold) #"\033[1;31m"
+GREEN=$(tput setaf 2; tput bold) #"\033[1;32m"
+YELLOW=$(tput setaf 3; tput bold)
+ENDCOLOR=$(tput sgr0) #"\033[0m"
+
+# Format string for printf
+_printf="%-10s %-10s %-12s %-1s\n"
+
+printf "\n${YELLOW}NOTE${ENDCOLOR}: Using ${GREEN}sudo${ENDCOLOR} gives accurate results in case of xrdp connection.\n"
+
+# Print header
+printf "\n${YELLOW}${_printf}${ENDCOLOR}" PID USERNAME CONNECTION STATUS
+
+# Process active xrdp (Xorg) sessions
+ps h -C Xorg -o user:20,pid,lstart,cmd | grep xrdp | while read username pid xorg_cmd; do
+    ss -ep 2>/dev/null | grep '/xrdp_display' | grep -q pid\=${pid}, && status="${GREEN}active${ENDCOLOR}" || status="${RED}disconnected${ENDCOLOR}"
+    printf "${_printf}" ${pid} "${username}" "xrdp" "${status}"
+done
+
+# Process active ssh sessions from 'w' command
+w -h --pids | grep 'sshd' | awk '{split($7, pids, "/"); print $1, $2, pids[1]}' | while read user ip pid; do
+    status="${GREEN}active${ENDCOLOR}"
+    printf "${_printf}" "${pid}" ${user} "ssh" "${status} from ${GREEN}${ip}${ENDCOLOR}"
+done
+
+echo ""
